64397b8653eeccfb5ebff4ceafab47d1b796c46b
Pankaj <Pankaj@DESKTOP-M4FDRAE>
2025-10-17 09:17:45 +0530

    done

--- Selenium_Learning/src/test/java/Learning/All_practice_E2E.java
+++ Selenium_Learning/src/test/java/Learning/All_practice_E2E.java
@@ -1,6 +1,11 @@
 package Learning;
 
+import java.io.File;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -11,7 +16,7 @@ public class All_practice_E2E {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		ChromeDriver driver = new ChromeDriver();
+		EdgeDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://demowebshop.tricentis.com/login");
 		driver.findElement(By.linkText("Register")).click();
@@ -19,7 +24,7 @@ public class All_practice_E2E {
 		driver.findElement(By.xpath("//label[normalize-space()='Male']")).click();
 		driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("Raj");
 		driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("Jain");
-		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("raj3232329@gmail.com");
+		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("raj3232356@gmail.com");
 		driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("Raj@1234");
 		driver.findElement(By.xpath("//input[@id='ConfirmPassword']")).sendKeys("Raj@1234");
 		Thread.sleep(5000);
@@ -37,7 +42,32 @@ public class All_practice_E2E {
 		driver.findElement(By.xpath("//input[@id='BillingNewAddress_Address1']")).sendKeys("Villa 1 , Phase 1");
 		driver.findElement(By.xpath("//input[@id='BillingNewAddress_ZipPostalCode']")).sendKeys("111111");
 		driver.findElement(By.xpath("//input[@id='BillingNewAddress_PhoneNumber']")).sendKeys("8687878787");
+		
 		driver.findElement(By.xpath("//input[@onclick='Billing.save()']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@onclick='Shipping.save()']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@class='button-1 shipping-method-next-step-button']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@class='button-1 payment-method-next-step-button']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@class='button-1 payment-info-next-step-button']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@class='button-1 confirm-order-next-step-button']")).click();
+		Thread.sleep(2000);
+		
+		TakesScreenshot sc=(TakesScreenshot)driver;
+		File source=sc.getScreenshotAs(OutputType.FILE);
+		File target=new File("C:\\Users\\Pankaj\\git\\repository\\Selenium_Learning\\Screenshot_exp\\E2E_Assignment.jpeg");
+		
+		source.renameTo(target);
+		Thread.sleep(6000);
+		driver.quit();
+		
+		
+		
+		
+		
 		
 		
 
